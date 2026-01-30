861ce1a140c9f384b7b66ececeef5458a1a742fc
joe-238 <171643026+joe-238@users.noreply.github.com>
2026-01-30 11:07:12 -0500

    classes

--- models/user.ts
+++ models/user.ts
@@ -1,7 +1,29 @@
 export class User {
-  id: Number; //osis
-  name: String;
-  grade: Number;
-  email: String;
-  isAdmin: Boolean;
+  constructor(
+    //runs automatically when you create a new object from a class
+    public id: number,
+    public name: string,
+    public grade: number,
+    public email: string,
+    public isAdmin: boolean,
+    private password: string
+  ) {}
+  joinEvent(userId: number) {
+    console.log(`${userId} joined this event lalala`);
+  }
+}
+
+export class AdminUser extends User {
+  constructor(
+    id: number,
+    name: string,
+    grade: number,
+    email: string,
+    password: string
+  ) {
+    super(id, name, grade, email, true, password);
+  }
+  deleteUser(userId: number) {
+    console.log(`Deleted user ${userId}`);
+  }
 }
