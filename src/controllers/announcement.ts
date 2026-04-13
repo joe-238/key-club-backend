5826f2b273797a4c62b0fe47ad04d15722743d7e
joe-238 <171643026+joe-238@users.noreply.github.com>
2026-04-13 11:11:59 -0400

    ddsad

--- src/controllers/announcement.ts
+++ src/controllers/announcement.ts
@@ -4,7 +4,58 @@ import * as announcementModel from "../models/announcement";
 export const createAnnouncement = async (req: Request, res: Response) => {
   try {
     const { status, publisher, title, description } = req.body;
+    if (!status || !publisher || !title || !description) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+    const announcement = await announcementModel.announcement.create({
+      status,
+      publisher,
+      title,
+      description,
+    });
+    res.status(201).json({
+      message: "Announcement created successfully",
+      announcement,
+    });
   } catch (error) {
     res.status(500).json({ message: "Failed to create announcement" });
+  }
+};
+
+export const getAnnouncements = async (req: Request, res: Response) => {
+  try {
+    const id = req.query.id;
+    if (!id) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+    const announcement = await announcementModel.announcement.findById(id);
+    if (!announcement) {
+      return res.status(404).json({
+        message: "Announcement not found",
+      });
+    }
+    res.status(200).json({ message: "Event found", announcement });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to retrieve announcements" });
+  }
+};
+
+export const deleteAnnouncement = async (req: Request, res: Response) => {
+  try {
+    const id = req.query.id;
+    if (!id) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+    const announcement = await announcementModel.announcement.findByIdAndDelete(
+      id
+    );
+    if (!announcement) {
+      return res.status(404).json({
+        message: "Announcement not found",
+      });
+    }
+    res.status(200).json({ message: "Announcement deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to delete announcement" });
   }
 };
